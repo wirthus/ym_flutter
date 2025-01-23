89249eb3555d9df060f03081716a0930671bd674
W <wirthus@mail.ru>
2025-01-23 23:11:01 +0300

    x

--- lib/presenter/pages/home/home.dart
+++ lib/presenter/pages/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:yagodmarket/presenter/route/route.gr.dart';
+import 'package:yagodmarket/utils/s.dart';
 
 @RoutePage()
 class HomePage extends StatefulWidget {
@@ -25,35 +26,47 @@ class _HomePageState extends State<HomePage> {
         builder: (context, child, controller) {
           final tabsRouter = AutoTabsRouter.of(context);
           return Scaffold(
-            body: child,
-            appBar: AppBar(
-              title: Text(context.topRoute.name),
-              leading: AutoLeadingButton(),
-              bottom: TabBar(
-                controller: controller,
-                tabs: const [
-                  Tab(text: '1', icon: Icon(Icons.abc)),
-                  Tab(text: '2', icon: Icon(Icons.abc)),
-                  Tab(text: '3', icon: Icon(Icons.abc)),
-                ],
-              ),
-            ),
+            body: Text("123"),
+            // appBar: AppBar(
+            //   title: Text(context.topRoute.name),
+            //   leading: AutoLeadingButton(),
+            //   bottom: TabBar(
+            //     controller: controller,
+            //     tabs: const [
+            //       Tab(text: '1', icon: Icon(Icons.abc)),
+            //       Tab(text: '2', icon: Icon(Icons.abc)),
+            //       Tab(text: '3', icon: Icon(Icons.abc)),
+            //     ],
+            //   ),
+            // ),
             bottomNavigationBar: BottomNavigationBar(
               currentIndex: tabsRouter.activeIndex,
               onTap: tabsRouter.setActiveIndex,
-              items: const [
+              items: [
                 BottomNavigationBarItem(
-                    icon: Icon(Icons.search), label: 'Search'),
+                  icon: Icon(Icons.search),
+                  label: S.of(context).page_search_title,
+                ),
                 BottomNavigationBarItem(
-                    icon: Icon(Icons.home), label: 'Adverts'),
+                  icon: Icon(Icons.home),
+                  label: S.of(context).page_adverts_title,
+                ),
                 BottomNavigationBarItem(
-                    icon: Icon(Icons.home), label: 'Points'),
+                  icon: Icon(Icons.home),
+                  label: S.of(context).page_points_title,
+                ),
                 BottomNavigationBarItem(
-                    icon: Icon(Icons.home), label: 'Favorites'),
+                  icon: Icon(Icons.home),
+                  label: S.of(context).page_favorites_title,
+                ),
                 BottomNavigationBarItem(
-                    icon: Icon(Icons.home), label: 'Subscriptions'),
+                  icon: Icon(Icons.home),
+                  label: S.of(context).page_subscriptions_title,
+                ),
                 BottomNavigationBarItem(
-                    icon: Icon(Icons.home), label: 'Profile'),
+                  icon: Icon(Icons.home),
+                  label: S.of(context).page_profile_title,
+                ),
               ],
             ),
           );
